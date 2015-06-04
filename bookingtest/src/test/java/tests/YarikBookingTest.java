fa4b6f69fe46a98effc8ee1fe2102ebe2b3748ee
ysparrow <ysparrow@gmail.com>
2015-06-04 17:19:03 +0300

    Added test

--- bookingtest/src/test/java/tests/YarikBookingTest.java
+++ bookingtest/src/test/java/tests/YarikBookingTest.java
@@ -3,10 +3,19 @@
  */
 package tests;
 
+import org.testng.annotations.Test;
+
 /**
  * @author y.pernerovskyy
  *
  */
 public class YarikBookingTest {
+
+
+    @Test
+    public void simpleTest() {
+	System.out.println("I'm in");
+    }
+    
 
 }
