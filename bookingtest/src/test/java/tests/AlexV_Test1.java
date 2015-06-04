0e2c26fd0eb22e229827442a1454e0e8f78f588a
AlexV <James_Z@ukr.net>
2015-06-04 19:57:19 +0300

    fdshgfvcbcv

---
+++ bookingtest/src/test/java/tests/AlexV_Test1.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class AlexV_Test1 {
+	public class YarikBookingTest {
+
+
+	    @Test
+	    public void simpleTest1sdfvb() {
+		System.out.println("I'm in");
+	    }
+}
+}
