523153ad717062aba7846f287d434c0c11c29f5d
ysparrow <ysparrow@gmail.com>
2015-06-06 23:56:53 +0300

    fix

--- bookingtest/src/test/java/tests/YarikBookingTest.java
+++ bookingtest/src/test/java/tests/YarikBookingTest.java
@@ -36,7 +36,7 @@ public class YarikBookingTest extends DriverSetup {
     @Features("Demo")
     @Stories("Demo test")
     public void testDemo() throws Exception {
-	driver.findElement(By.id("sss")).click();
+	
     }
 
    
